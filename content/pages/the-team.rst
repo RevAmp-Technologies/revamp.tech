4fe79cf0bc9ebffdd10aef9ffc60d97a0fc12054
Michael Mossinsohn <consistent1@gmail.com>
2016-06-30 23:05:41 +0300

    A repository for the revamp.tech website - First commit based on the Pelican static site generator, written in Python.

---
+++ content/pages/the-team.rst
@@ -0,0 +1,45 @@
+The Team
+########
+:date: 2016-06-22 20:33
+:author: RevAmp Technologies
+:slug: the-team
+:status: published
+
+
+**RevAmp Technologies is a small Israeli startup.**
+   **Our founders are:**
+
+
+
+Solomon Khmelnik, Ph.D., CSO
+____________________________
+|Solomon Khmelnik, Ph.D.|
+
+Solomon is the man behind the science and the technology. His expertise, thoroughness, and dedication made these scientific and technological domains accessible. Solomon has a Ph.D. in technical sciences, and his areas of expertise include electrical engineering, designing hardware for high-speed arithmetic computations, and (search method for global solutions of) the Navier-Stokes equations.
+
+
+Inna Doubson, COO
+_________________
+|Inna Doubson, COO|
+
+Inna, Solomon's daughter, assists him in many ways. Inna has been
+collaborating with Solomon for years, both in the field of hardware
+design and in his physics related projects.
+
+
+Michael Dan Mossinsohn, CEO
+___________________________
+|Michael Dan Mossinsohn, CEO|
+
+Michael is an IT specialist who spent years studying alternative methods
+for energy 'generation'. He is dedicated to bringing about a future of
+sustainable abundance for humankind.
+
+
+.. |Solomon Khmelnik, Ph.D.| image:: ../wp-content/uploads/Solik-Khmelnik.bmp
+   :class: image team
+.. |Inna Doubson, COO| image:: ../wp-content/uploads/inna2-e1428328062687.jpg
+   :class: image team
+.. |Michael Dan Mossinsohn, CEO| image:: ../wp-content/uploads/mics_pic.jpg
+   :class: image team
+
---
+++ pelicanconf.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'RevAmp Technologies'
+SITENAME = u'RevAmp Technologies'
+SITEURL = u'hhtp://revamp.tech'
+
+PATH = 'content'
+STATIC_PATHS = ['images',
+                'fonts',
+                'css',
+                'js',
+                ]
+
+PAGE_DIR = 'pages'
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+
+ARTICLE_PATHS = ['blog']
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{slug}.html'
+
+
+
+TIMEZONE = 'Asia/Jerusalem'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+
+THEME = "twenty-html5up"
+
+# Needed by the twenty-html5up theme
+def sidebar(value):
+  if value.startswith('archives') or value.startswith('category'):
+    return 'right-sidebar'
+  elif value == 'index':
+    return 'index'
+  else:
+    return 'no-sidebar'
+
+JINJA_FILTERS = { 'sidebar': sidebar }
+
+# Added for revamp.tech
+TYPOGRIFY = True
+MENUITEMS = [('Home', 'index.html')]
+DIRECT_TEMPLATES = ['index']
