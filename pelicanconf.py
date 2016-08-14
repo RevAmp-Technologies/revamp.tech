fa9b2d11e63a6fcf73ecd88f34969b3d448dfd16
Michael Mossinsohn <consistent1@gmail.com>
2016-08-14 04:06:30 +0300

    Added social icons (Facebook, Twitter, and GitHub ) and removed the default pagination.

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,13 +44,14 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('facebook', 'https://www.facebook.com/RevAmpTech'),
+          ('twitter', 'https://twitter.com/revamptech'),
+          ('github', 'https://revamp-technologies.github.io/'),)
 
-DEFAULT_PAGINATION = True
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 
 THEME = "twenty-html5up"
